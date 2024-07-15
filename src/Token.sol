0ec6b671abd6e82fca7f5c15e061a9a6c33cfe86
jmakwana01 <jaymakwanacodes@gmail.com>
2024-07-15 14:40:08 +0530

    first commit

---
+++ src/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor() ERC20("OurToken", "OT") {
+        _mint(msg.sender, 100e18);
+    }
+
+}
---
+++ test/token_test.t.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {DeployToken} from "../script/DeployToken.s.sol";
+import {Token} from "../src/Token.sol";
+import {Test, console} from "forge-std/Test.sol";
+
+interface MintableToken {
+    function mint(address, uint256) external;
+}
+
+contract OurTokenTest is Test {
+    uint256 BOB_STARTING_AMOUNT = 100 ether;
+    uint256 public constant INITIAL_SUPPLY = 1_000_000 ether; // 1 million tokens with 18 decimal places
+
+    Token public ourToken;
+    DeployToken public deployer;
+    address public deployerAddress;
+    address bob;
+    address alice;
+
+    function setUp() public {
+        deployer = new DeployToken();
+        
+            ourToken = new Token();
+            ourToken.transfer(msg.sender, INITIAL_SUPPLY);
+        
+
+        bob = makeAddr("bob");
+        alice = makeAddr("alice");
+
+        vm.prank(msg.sender);
+        ourToken.transfer(bob, BOB_STARTING_AMOUNT);
+    }
+
+    function testInitialSupply() public view {
+        assertEq(ourToken.totalSupply(), deployer.INITIAL_SUPPLY());
+    }
+
+    function testUsersCantMint() public {
+        vm.expectRevert();
+        MintableToken(address(ourToken)).mint(address(this), 1);
+    }
+
+    function testAllowances() public {
+        uint256 initialAllowance = 1000;
+
+        // Bob approves Alice to spend tokens on his behalf
+
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+        uint256 transferAmount = 500;
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, transferAmount);
+        assertEq(ourToken.balanceOf(alice), transferAmount);
+        assertEq(ourToken.balanceOf(bob), BOB_STARTING_AMOUNT - transferAmount);
+    }
+
+}
